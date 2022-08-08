9d0d5340c611d28a214682beaca7e218f3538695
Bill Bensing <bill@billbensing.com>
2022-08-08 17:28:44 -0400

    removed interface, became unnecessary

--- usecase/register_public_domain/RequestForRegistration.go
+++
@@ -1,7 +0,0 @@
-package register_public_domain
-
-// RequestForRegistration is the interface which defines the behaviors expected when invoking the use case to
-//register a public domain
-type RequestForRegistration interface {
-	Domain() string
-}
