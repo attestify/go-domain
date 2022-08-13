bac654e369d0a325ea2aeaf8cb08181de0575c86
Bill Bensing <bill@billbensing.com>
2022-08-12 23:00:49 -0400

    Added sad path test for nil gateway

--- usecase/assign_role/AssignRole.go
+++ usecase/assign_role/AssignRole.go
@@ -1,10 +1,17 @@
 package assign_role
 
+import "github.com/attestify/go-kernel/error/internal_error"
+
 type AssignRole struct {
 	gateway AssignRoleGateway
 }
 
 func New(gateway AssignRoleGateway) (AssignRole, error) {
+
+	if gateway == nil {
+		return AssignRole{}, internal_error.New("the provided AssignRoleGateway is nil, please provide a valid instance of an AssignRoleGateway")
+	}
+
 	return AssignRole{
 		gateway: gateway,
 	}, nil
--- usecase/assign_role/AssignRole_test.go
+++ usecase/assign_role/AssignRole_test.go
@@ -1,7 +1,9 @@
 package assign_role_test
 
 import (
+	"errors"
 	"github.com/attestify/go-domain/usecase/assign_role"
+	"github.com/attestify/go-kernel/error/internal_error"
 	"testing"
 )
 
@@ -9,6 +11,7 @@ func setup(t *testing.T) {
 	t.Parallel()
 }
 
+/** Happy Path **/
 
 // Given an instance of a AssignRoleGateway is provided
 // When the AssignRole use case is instantiated
@@ -21,6 +24,37 @@ func Test_Instantiate_AssignRole_Successfully(t *testing.T) {
 		t.Errorf("An error was returned when no error was expected: \n %s", err.Error())
 	}
 }
+
+/** Sad Path **/
+
+// Given an nil instance of a AssignRoleGateway is provided
+// When the AssignRole use case is instantiated
+// Then an InternalError with the text "the provided AssignRoleGateway is nil, please provide a valid instance of an AssignRoleGateway" should be returned
+func Test_Instantiate_AssignRole_With_Nil_AssignRoleGateway(t *testing.T) {
+	setup(t)
+	// Assemble
+	var assignRoleGateway assign_role.AssignRoleGateway = nil
+
+	// Act
+	_, err := assign_role.New(assignRoleGateway)
+
+	// Assert
+	if err == nil {
+		t.Fatalf("An error was expected, although no error was returned.")
+	}
+
+	if !errors.As(err, &internal_error.InternalError{}) {
+		t.Errorf("did not get the epected error of type InternalError")
+	}
+
+	actualMessage := err.Error()
+	expectedMessage := "the provided AssignRoleGateway is nil, please provide a valid instance of an AssignRoleGateway"
+	if expectedMessage != actualMessage {
+		t.Errorf("The returned error message was not expected: \n Expected: %s \n Actual %s", expectedMessage, actualMessage)
+	}
+}
+
+
 
 // todo -- AssignRole Invocation
 // Given the user id of "[x]" is provided,
